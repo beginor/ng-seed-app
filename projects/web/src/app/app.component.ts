dad04f6a8f7bd8230206904d7e69a85fdcd1367d
beginor <beginor@qq.com>
2024-05-04 18:50:58 +0800

    dynamic load nz-icon

--- projects/web/src/app/app.component.ts
+++ projects/web/src/app/app.component.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import { RouterOutlet, RouterLink } from '@angular/router';
+import { NzIconService } from 'ng-zorro-antd/icon';
 
 @Component({
     selector: 'app-root',
@@ -9,5 +10,9 @@ import { RouterOutlet, RouterLink } from '@angular/router';
     styleUrl: './app.component.css'
 })
 export class AppComponent {
-    title = 'web';
+
+    constructor(nzIconService: NzIconService) {
+        nzIconService.changeAssetsSource('assets/icons/antd/');
+    }
+
 }
