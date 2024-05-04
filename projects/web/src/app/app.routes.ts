0ee54fecf6abf0a0f2093e0b40f7bc37c1cd649d
beginor <beginor@qq.com>
2024-05-04 22:29:35 +0800

    add page not found component

--- projects/web/src/app/app.routes.ts
+++ projects/web/src/app/app.routes.ts
@@ -3,10 +3,20 @@ import { Routes } from '@angular/router';
 export const routes: Routes = [
     {
         path: 'home',
+        title: 'Home',
         loadChildren: () => import('./home/home.routes').then(m => m.routes)
     },
     {
         path: 'about',
+        title: 'About',
         loadChildren: () => import('./about/about.routes').then(m => m.routes)
+    },
+    {
+        path: '', redirectTo: '/home', pathMatch: 'full'
+    },
+    {
+        path: '**',
+        title: 'Page not found!',
+        loadComponent: () => import('./not-found/not-found.component').then(m => m.NotFoundComponent)
     }
 ];
---
+++ projects/web/src/app/not-found/not-found.component.ts
@@ -0,0 +1,14 @@
+import { Component } from '@angular/core';
+
+@Component({
+    selector: 'app-not-found',
+    standalone: true,
+    imports: [],
+    template: `
+        <h2>Page not found!</h2>
+    `,
+    styles: [':host { display: block; }']
+})
+export class NotFoundComponent {
+
+}
