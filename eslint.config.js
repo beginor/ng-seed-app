b5bc5f76f4d7d8df0cd5a8461946cdda6a772ccc
beginor <beginor@qq.com>
2025-01-17 15:20:19 +0800

    fix eslint

--- eslint.config.js
+++ eslint.config.js
@@ -1,11 +1,15 @@
 // @ts-check
-const eslint = require("@eslint/js");
-const tseslint = require("typescript-eslint");
-const angular = require("angular-eslint");
+import eslint from '@eslint/js';
+import stylistic from '@stylistic/eslint-plugin'
+import tseslint from 'typescript-eslint';
+import angular from 'angular-eslint';
 
-module.exports = tseslint.config(
+export default tseslint.config(
   {
-    files: ["**/*.ts"],
+    files: ["projects/**/*.ts"],
+    plugins: {
+      '@stylistic': stylistic
+    },
     extends: [
       eslint.configs.recommended,
       ...tseslint.configs.recommended,
@@ -13,7 +17,57 @@ module.exports = tseslint.config(
       ...angular.configs.tsRecommended,
     ],
     processor: angular.processInlineTemplates,
-    rules: {},
+    rules: {
+      'no-debugger': ['error'],
+      'no-console': ['warn', { allow: ['error'] }],
+      'no-prototype-builtins': ['off'],
+      '@stylistic/max-len': [
+        'error', {
+          code: 80,
+          tabWidth: 4,
+          ignoreComments: true,
+          ignoreTrailingComments: true,
+          ignoreUrls: true,
+          ignoreStrings: true,
+          ignoreTemplateLiterals: true,
+          ignoreRegExpLiterals: true,
+        }
+      ],
+      '@stylistic/quotes': ['error', 'single'],
+      '@typescript-eslint/no-explicit-any': ['warn'],
+      '@typescript-eslint/explicit-function-return-type': [
+        'error',
+        {
+          allowExpressions: true,
+          allowTypedFunctionExpressions: true,
+          allowHigherOrderFunctions: true,
+          allowDirectConstAssertionInArrowFunctions: true,
+          allowConciseArrowFunctionExpressionsStartingWithVoid: true,
+          allowFunctionsWithoutTypeParameters: true,
+          allowedNames: [],
+          allowIIFEs: true,
+        },
+      ],
+      '@typescript-eslint/explicit-member-accessibility': [
+        'error',
+        {
+          accessibility: 'explicit',
+          ignoredMethodNames: [],
+          overrides: {
+            accessors: 'explicit', constructors: 'off', methods: 'explicit',
+            parameterProperties: 'explicit', properties: 'explicit',
+          }
+        }
+      ],
+      "@angular-eslint/directive-selector": [
+        "error",
+        { type: "attribute", style: "camelCase" }
+      ],
+      "@angular-eslint/component-selector": [
+        "error",
+        { type: "element", style: "kebab-case" }
+      ]
+    },
   },
   {
     files: ["**/*.html"],
@@ -21,6 +75,18 @@ module.exports = tseslint.config(
       ...angular.configs.templateRecommended,
       ...angular.configs.templateAccessibility,
     ],
-    rules: {},
+    rules: {
+      '@angular-eslint/template/click-events-have-key-events': ['off'],
+      '@angular-eslint/template/interactive-supports-focus': ['off']
+    },
+  },
+  {
+    ignores: [
+      '.angular',
+      '.idea',
+      '.vscode',
+      'dist',
+      'node_modules',
+    ]
   }
 );
