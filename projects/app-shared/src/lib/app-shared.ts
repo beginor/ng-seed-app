5ecd60c9b8235d72346845de496d86bed908b76b
beginor <beginor@qq.com>
2025-08-23 19:33:23 +0800

    angular-cli 20.2.0

--- projects/app-shared/src/lib/app-shared.ts
+++ projects/app-shared/src/lib/app-shared.ts
@@ -10,6 +10,6 @@ import { Component } from '@angular/core';
   `,
   styles: ``
 })
-export class AppSharedComponent {
+export class AppShared {
 
 }
--- projects/app-shared/src/public-api.ts
+++ projects/app-shared/src/public-api.ts
@@ -2,5 +2,4 @@
  * Public API Surface of app-shared
  */
 
-export * from './lib/app-shared.service';
-export * from './lib/app-shared.component';
+export * from './lib/app-shared';
